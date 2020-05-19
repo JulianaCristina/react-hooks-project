18427362de36b1f8c0cf232b47fe1ad1d71b5a9c
juliana <julianacristina780@gmail.com>
2020-05-19 11:10:51 -0300

    Array destructuring

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,7 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-    const inputState = useState({title: '', amount: ''})
+    const [inputState, setInputState] = useState({title: '', amount: ''})
     const submitHandler = event => {
         event.preventDefault();
 
@@ -18,10 +18,10 @@ const IngredientForm = React.memo(props => {
                         <label htmlFor="title">Name</label>
                         <input type="text"
                                id="title"
-                               value={inputState[0].title}
+                               value={inputState.title}
                                onChange={event => {
                                    const newTitle = event.target.value
-                                   inputState[1](prevInputState => ({
+                                   setInputState(prevInputState => ({
                                            title: newTitle,
                                            amount: prevInputState.amount
                                        })
@@ -32,12 +32,12 @@ const IngredientForm = React.memo(props => {
                         <label htmlFor="amount">Amount</label>
                         <input type="number"
                                id="amount"
-                               value={inputState[0].amount}
+                               value={inputState.amount}
                                onChange={event => {
                                    const newAmount = event.target.value
-                                   inputState[1](prevInputState => ({
+                                   setInputState(prevInputState => ({
                                        amount: newAmount,
-                                       title: inputState[0].title
+                                       title: prevInputState.title
                                    }))
                                }}
                         />
